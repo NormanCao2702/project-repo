fe6739efd6712ce93901c5befa3663ff8698eb05
Tran Quang Ngoc Cao <118385554+NormanCao2702@users.noreply.github.com>
2023-07-18 15:38:51 -0700

    Update UserController.java-modify return statement-remove slash before users

--- src/main/java/com/swift_po/swift_po/controllers/UserController.java
+++ src/main/java/com/swift_po/swift_po/controllers/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
 
     @GetMapping("/signup")
     public String getSignUp() {
-        return "/users/signup";
+        return "users/signup";
     }
     
     @GetMapping("/pr")
@@ -60,7 +60,7 @@ public class UserController {
         if (!existingUsers.isEmpty()) {
             String error = "Email already in use. Please choose a different email.";
             model.addAttribute("error", error);
-            return "/users/signup";
+            return "users/signup";
         }
         // Password policy validation
         if (!isStrongPassword(newPwd)) {
@@ -71,7 +71,7 @@ public class UserController {
         User tempuser = new User( newEmail, newName, newPwd, newuserType);
         UserServices.registerUser(tempuser);
         response.setStatus(201);
-        return "/users/login";
+        return "users/login";
     }
     
     // Helper method to check password strength
@@ -84,11 +84,11 @@ public class UserController {
     public String getLogin(Model model, HttpServletRequest request, HttpSession session){
         User user = (User) session.getAttribute("session_user");
         if (user == null){
-            return "/users/login";
+            return "users/login";
         }
         else {
             model.addAttribute("user",user);
-            return "/users/form";
+            return "users/form";
         }
     }
 
@@ -96,11 +96,11 @@ public class UserController {
     public String getForm(Model model, HttpServletRequest request, HttpSession session){
         User user = (User) session.getAttribute("session_user");
         if (user == null)   {
-            return "/users/login";
+            return "users/login";
         }
         else {
             model.addAttribute("user",user);
-            return "/users/form";
+            return "users/form";
         }
     }
 
@@ -108,11 +108,11 @@ public class UserController {
     public String showForm(Model model, HttpServletRequest request, HttpSession session){
         User user = (User) session.getAttribute("session_user");
         if (user == null)   {
-            return "/users/login";
+            return "users/login";
         }
         else {
             model.addAttribute("user",user);
-            return "/users/formpr";
+            return "users/formpr";
         }
     }
 
@@ -120,11 +120,11 @@ public class UserController {
     public String getPr(Model model, HttpServletRequest request, HttpSession session){
         User user = (User) session.getAttribute("session_user");
         if (user == null)   {
-            return "/users/login";
+            return "users/login";
         }
         else {
             model.addAttribute("user",user);
-            return "/users/pr";
+            return "users/pr";
         }
     }
 
@@ -132,11 +132,11 @@ public class UserController {
     public String getSrc(Model model, HttpServletRequest request, HttpSession session){
         User user = (User) session.getAttribute("session_user");
         if (user == null)   {
-            return "/users/login";
+            return "users/login";
         }
         else {
             model.addAttribute("user",user);
-            return "/users/srcJustification";
+            return "users/srcJustification";
         }
     }
 
@@ -148,7 +148,7 @@ public class UserController {
         List<User> userlist = userRepo.findByEmailAndPassword(uname, pwd);
         if (userlist.isEmpty()){
             model.addAttribute("error", "Invalid username or password");
-            return "/users/login";
+            return "users/login";
         }
         else {
             // success
@@ -162,7 +162,7 @@ public class UserController {
     @GetMapping("/logout")
     public String destroySession(HttpServletRequest request){
         request.getSession().invalidate();
-        return "/users/login";
+        return "users/login";
     }
 }
 
