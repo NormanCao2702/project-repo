4ea5a43dbbae6282883a7a6494913547aac824a4
NormanCao2702 <norman.cao2702@gmail.com>
2023-07-18 11:36:13 -0700

    updating testing

---
+++ target/classes/static/js/script.js
@@ -0,0 +1,14 @@
+function dropdown() {
+    document.querySelector('#pomenu').classList.toggle('hidden');
+    document.querySelector('#arrowdown').classList.toggle('rotate-0');
+
+}
+
+
+function OpenNav() {
+    document.querySelector('#sidenav').classList.toggle('w-[6%]');
+    document.querySelector('#navcontent').classList.toggle('hidden');
+    document.querySelector('#logouttext').classList.toggle('hidden');
+
+
+}
