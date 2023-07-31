fba734496d4a4b00c131762d39c79f97f4a802b5
NormanCao2702 <norman.cao2702@gmail.com>
2023-07-31 14:04:09 -0700

    updating VMO user interface, database changed, repo added, still not implemented the deny button when viewing pending orders, not yet done the approved process, general done with demonstrate all IS user and all their submitted form in VMO user interface

--- src/main/java/com/swift_po/swift_po/controllers/VMOController.java
+++ src/main/java/com/swift_po/swift_po/controllers/VMOController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.swift_po.swift_po.models.Request;
 import com.swift_po.swift_po.models.RequestReposiory;
 import com.swift_po.swift_po.models.User;
 import com.swift_po.swift_po.models.userRepo;
@@ -24,7 +27,26 @@ public class VMOController {
     public String viewForms(Model model) {
         List<User> usersWithForms = userRepo.findByUserType("IS User");
         model.addAttribute("usersWithForms", usersWithForms);
-        return "users/pending"; // Return the Thymeleaf template view name
+        return "users/pending"; 
+    }
+
+    @GetMapping("/user-forms")
+    public String showUserForms(@RequestParam("userId") int userId, Model model) {
+        User user = userRepo.findById(userId).orElse(null);
+        if (user != null) {
+            model.addAttribute("user", user);
+            return "users/vmoFormreview"; 
+        }
+        return "error"; 
+    }
+
+    @GetMapping("/VMOviewForm/{rid}")
+    public String viewFormDetails(@PathVariable int rid, Model model) {
+        Request form = requestRepo.findById(rid).get(0);
+
+        model.addAttribute("form", form);
+
+        return "users/VMOform_details";
     }
 
 }
